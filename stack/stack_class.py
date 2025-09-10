b804aa11fcacc5a7da13b833fb2db21bf17f3a1b
김현서 <seoshio@naver.com>
2025-09-10 16:23:03 +0900

    Fix bug in stack_class code

--- stack/stack_class.py
+++ stack/stack_class.py
@@ -1,42 +1,38 @@
-
-
-
-
 class ArrayStack :
     def __init__(self, capacity):
         self.capacity = capacity
         self.array = [None] * self.capacity
         self.top = -1
 
-        def is_empty(self):
-            return self.top == -1
+    def is_empty(self):
+        return self.top == -1
         
-        def is_full(self):
-            return self.top == self.capacity - 1
+    def is_full(self):
+        return self.top == self.capacity - 1
         
-        def push(self, item):
-            if not self.is_full():
+    def push(self, item):
+        if not self.is_full():
                 self.top += 1
                 self.array[self.top] = item
                 print(f"PUSH: {item!r} -> stack = {self.array[:self.top +1]}")
 
-            else:
+        else:
                 raise OverflowError("Stack Overflow")
 
-        def pop(self):
-            if not self.is_empty():
-                item = self.array[self.top]
-                self.array[self.top] = None
-                self.top -= 1
-                print(f"POP: {item!r} -> stack = {self.array[:self.top +1]}")
-                
-            else: 
-                raise IndexError("Stack underflow")
+    def pop(self):
+        if not self.is_empty():
+            item = self.array[self.top]
+            self.array[self.top] = None
+            self.top -= 1
+            print(f"POP: {item!r} -> stack = {self.array[:self.top +1]}")
+            return item
+        else: 
+            raise IndexError("Stack underflow")
             
-        def peek(self):
-            if not self.is_empty():
-                return self.array[self.top]
-            return None
+    def peek(self):
+        if not self.is_empty():
+            return self.array[self.top]
+        return None
     
     def size(self):
         return self.top + 1
